a9a3c765247e33f139e1593b1eed8e322952ae76
Derek Crosson <derekcrosson18@gmail.com>
2022-05-19 03:26:46 +0200

    feat: print text to screen

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,20 @@
 
 use core::panic::PanicInfo;
 
+static WELCOME_MESSAGE: &[u8] = b"Rustic OS works!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in WELCOME_MESSAGE.iter().enumerate() {
+        // unsafe-ing for now just to test, will fix this later
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
