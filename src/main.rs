1fa7f9b0ce153f472e50cf47dc54be4868bec5b5
Derek Crosson <derekcrosson18@gmail.com>
2022-05-20 02:33:03 +0200

    feat: add println macro

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,7 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Rustic OS Works!").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", Here are some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Rustic OS Works{}", "!");
 
     loop {}
 }
